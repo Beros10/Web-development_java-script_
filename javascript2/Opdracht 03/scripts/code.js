d6c43ca0d684bfd8486f4860f5c72a1a2a568d6c
Beros10 <92428033+Beros10@users.noreply.github.com>
2022-03-13 12:25:41 +0100

    ihihi

---
+++ javascript2/Opdracht 03/scripts/code.js
@@ -0,0 +1,7 @@
+const setup = () => {
+    let pElement=document.getElementById("txtOutput");
+    pElement.innerHTML="Welkom!";
+// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+}
+window.addEventListener("load", setup);
+
