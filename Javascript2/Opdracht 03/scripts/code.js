69aa3816e4287db023ee01a02ca688352826b42e
Beros10 <92428033+Beros10@users.noreply.github.com>
2022-03-13 12:24:29 +0100

    knk

--- Javascript2/Opdracht 03/scripts/code.js
+++
@@ -1,7 +0,0 @@
-const setup = () => {
-    let pElement=document.getElementById("txtOutput");
-    pElement.innerHTML="Welkom!";
-// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
-}
-window.addEventListener("load", setup);
-
